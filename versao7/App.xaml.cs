e28d562a6d4e507aba60f00498a4963d6df952ed
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-10-02 16:40:09 -0300

    Initial commit

---
+++ versao7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace versao7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
